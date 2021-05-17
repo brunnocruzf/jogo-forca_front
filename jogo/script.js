66450bb7e5cabac7ff82c2d46ed60b0d42f9e100
bruno.cruzf@gmail.com <Gremio@10>
2021-05-16 21:45:41 -0300

    não repete palavras

--- jogo/script.js
+++ jogo/script.js
@@ -303,7 +303,7 @@ function buscaPalavra() {
 					i++;
 					console.log("valor de i"+i);
 					buscaPalavra();
-					if(i==40){
+					if(i == 30){
 						todasPerguntas = [];
 					}
 				} else {
